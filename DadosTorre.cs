d34d3a2c6ca49901b512b2598d487e3598525a03
Franck Wallace <franckwallace.fw@outlook.com>
2024-11-04 21:51:16 -0300

    Commit inicial

---
+++ DadosTorre.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comp_Torres
+{
+    internal class DadosTorre
+    {
+        public static void CadastrarDados()
+        {
+            // Pergunta sobre o tamanho da torre
+            int tamanhoTorre = ObterTamanhoQuadro();
+
+            if (tamanhoTorre == 0)
+            {
+                Console.WriteLine("Saindo da aplicação...");
+                Environment.Exit(0);
+            }
+
+            Console.WriteLine($"Tamanho da Torre selecionado: {tamanhoTorre}");
+        }
+
+        public int TamanhoQuadro { get; set; }
+        //public int Niveis { get; set; }
+
+        private static int ObterTamanhoQuadro()
+        {
+            int tamanho = -1;
+
+            while (true)
+            {
+                Console.WriteLine("\nQual o tamanho da torre:");
+                Console.WriteLine("1 - 100");
+                Console.WriteLine("2 - 125");
+                Console.WriteLine("3 - 155");
+                Console.WriteLine("4 - 175");
+                Console.WriteLine("0 - Sair");
+
+                string input = Console.ReadLine();
+
+                if (int.TryParse(input, out tamanho) && (tamanho >= 0 && tamanho <= 4))
+                {
+                    if (tamanho == 0)
+                    {
+                        return 0; // Sai do programa se a opção for 0
+                    }
+                    return tamanho;
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Opção inválida, tente novamente.");
+                }
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comp_Torres
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            while (true)
+            {
+                
+                Console.WriteLine("\t---Composição de Torres---\n");
+                Console.WriteLine("Bem-vindo(a). Escolha uma das opções abaixo para prosseguir:");
+                Console.WriteLine("1 - Criar uma nova composição de torre;");
+                Console.WriteLine("0 - Sair.");
+
+                string opcao = Console.ReadLine();
+
+                if (opcao == "1")
+                {
+                    DadosTorre.CadastrarDados();
+                    //Console.ReadKey();
+                }
+
+                else if (opcao == "0")
+                {
+                    Console.WriteLine("Fechando o programa.");
+                    break;
+                }
+
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Opção inválida, tente novamente!\n");
+
+                }
+
+            }
+
+
+        }
+
+
+
+
+    }
+}
+
